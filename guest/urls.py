768b9259c2649e73e704dde4de9cfefefd7d931a
kunpeng <kunpengv5@qq.com>
2017-11-15 18:02:09 +0800

    新增视图模板

--- guest/urls.py
+++ guest/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from sign import views   #导入sign应用views文件
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^index/$',views.index),  #添加index/路径配置   
 ]
